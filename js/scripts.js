2ec71691082a69ee8e3862433bd1f715b49cca68
Austin Barr and Marie Dillard <austin@>
2018-06-06 15:13:46 -0700

    added everthing

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+var vowelOptions = ['a','e','i','o','u'];
+var consonantCount = [];
+
+var pigLatin = function(input) {
+  //creates array
+  var letters = input.split("");
+  var firstVowelPosition = 0;
+  //collects vowel status of first letter
+  var startVowel = vowelOptions.indexOf(letters[0]);
+  //if a consonant
+  if (startVowel === -1){
+    //please add logic to determine
+    for (i = 0; i < letters.length; i++) {
+      if (vowelOptions.indexOf(letters[i]) != -1) {
+        firstVowelPosition = i;
+        break;
+      }
+    }
+    var finalResult = input.slice(firstVowelPosition, input.length) + input.slice(0, firstVowelPosition);
+    return finalResult + "ay";
+  //else if a vowel
+  } else {
+    return input + "way";
+  }
+}
+
+
+$(function(){
+  $("form#pig-latin").submit(function(event){
+    event.preventDefault();
+    var english = $("input#english").val();
+    $("#result").text(pigLatin(english));
+})
+});
+
+
+
+    // if (first letter is vowel) {
+    //   result + "way"
+    // } else if (first letter is consonant) {
+    // cycle through and add
+    // until vowel
+    // if vowel
+    // break;
+    //}
+
+//   });
+//
+// });
